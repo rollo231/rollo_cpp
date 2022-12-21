98df7cf432321f38864c0478a6d9dc087422cf82
sangwon lee <231rollo@gmail.com>
2022-12-21 21:34:43 +0900

    del

--- 10week_coding_test/week0/1.2/1.2.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h> // 헤더파일 include
-
-using namespace std; // std 라는 네임스페이스 사용, 라이브러리 변수명 중복 방지
-string a; // 문자열 변수 a선언
-int main() {
-    cin >> a; // 변수 a를 입력 받는다.
-    cout << a << "\n"; // 변수 a를 출력
-    return 0; // main 함수 종료
-}
--- 10week_coding_test/week0/1.2/typedef.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-typedef int i; // 타입의 이름에 새로이 별칭으로 정의하고 실제 타입이름 대신 다른 별칭으로 사용
-int main() {
-    i a = 1;
-    cout << a << "\n";
-    return 0;
-}
