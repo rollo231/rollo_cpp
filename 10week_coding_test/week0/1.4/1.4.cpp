3eaa7e3e40410b67e696d5fad98e33495848ffba
sangwon lee <rollo@korda.im>
2022-12-21 09:56:00 +0900

    1.4

---
+++ 10week_coding_test/week0/1.4/1.4.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double a() {
+    cout << 1.2333 << "\n";
+}
+
+char c() {
+    char a = 'a';
+    cout << a << "\n";
+}
+
+void s() {
+    char s[10] = "gd";
+    string a = "gd";
+    cout << s << " " << a << "\n";
+}
+
+int main() {
+    a();
+    c();
+    s();
+    return 0;
+}
