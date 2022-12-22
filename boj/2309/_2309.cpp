04b6b8e79b4f4070e2b66a7256610078a86c0790
sangwon lee <rollo@korda.im>
2022-12-22 10:34:15 +0900

    live template fix

--- boj/2309/_2309.cpp
+++ boj/2309/_2309.cpp
@@ -18,8 +18,8 @@ void solve() {
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     for (int &i: a) {
         cin >> i;
