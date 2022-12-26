edf3d0a48682408533dadf4855e70e45eb8c38f9
sangwon lee <231rollo@gmail.com>
2022-12-26 22:38:50 +0900

    2559

---
+++ boj/2559/2559.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k, temp, psum[100001], ret = -1000000;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> temp;
+        psum[i] = psum[i - 1] + temp;
+    }
+
+    for (int i = k; i <= n; i++) {
+        ret = max(ret, psum[i] - psum[i - k]);
+    }
+
+    cout << ret << "\n";
+
+    return 0;
+}
